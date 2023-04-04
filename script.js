7a12f184f56a569433c14c77ea07481aa62d1c93
Gabriel <gabrielnovatoo@gmail.com>
2023-04-04 16:13:42 -0300

    Hours working

--- script.js
+++ script.js
@@ -9,5 +9,21 @@ console.log(hour)
 let clock = setInterval(
     function time(){
         let date_now = new Date();
-    }
+        let hr = date_now.getHours();
+        let min = date_now.getMinutes();
+        let sec = date_now.getSeconds();
+        console.log(sec)
+        if(hr < 10){
+            hr = "0" + hr;
+        }
+        if(min < 10){
+            min = "0" + min;
+        }
+        if(sec < 10){
+            sec = "0" + sec;
+        }
+        hour.textContent = hr;
+        minute.textContent = min;
+        seconds.textContent = sec;
+    }, 1000
 )
