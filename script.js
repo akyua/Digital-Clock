c32d1322391d0fe2686414466795d5fd27876ca9
Gabriel <gabrielnovatoo@gmail.com>
2023-04-04 16:05:40 -0300

    Create html and your stylish

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let seconds = document.getElementById('seconds');
+
+console.log(hour)
+
+let clock = setInterval(
+    function time(){
+        let date_now = new Date();
+    }
+)
